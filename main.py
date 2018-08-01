debc5dbf365756a95cb9b2a1af7afd24304ce3e4
Brian Harbron <bharbron@grubhub.com>
2018-08-01 10:39:25 -0500

    Brainstorming

---
+++ main.py
@@ -0,0 +1,17 @@
+def parse_and_replace(text):
+	"""
+	Parses the text for the first instance of a %TABLE_REFERNCE% and replaces it with the result from that table
+
+	Returns the updated string, or NoneType if there were no %TABLE_REFERENCE%[s]
+	"""
+	pass
+
+def roll_on_table(table):
+	"""
+	Fetch a random entry from the given table
+
+	If the table is a list, simply return a random entry
+
+	If the table is a dict, roll dice and lookup result
+	"""
+	pass
