c98e69d41995d736e463cb277b0d35c490497dc0
Brian Harbron <bharbron@grubhub.com>
2018-08-01 12:05:57 -0500

    More filling in of tables

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 def parse_and_replace(text):
-	"""
-	Parses the text for the first instance of a %TABLE_REFERNCE% and replaces it with the result from that table
+    """
+    Parses the text for the first instance of a %TABLE_REFERNCE% and replaces it with the result from that table
 
-	Returns the updated string, or NoneType if there were no %TABLE_REFERENCE%[s]
-	"""
-	pass
+    Returns the updated string, or NoneType if there were no %TABLE_REFERENCE%[s]
+    """
+    pass
+
 
 def roll_on_table(table):
-	"""
-	Fetch a random entry from the given table
+    """
+    Fetch a random entry from the given table
 
-	If the table is a list, simply return a random entry
+    If the table is a list, simply return a random entry
 
-	If the table is a dict, roll dice and lookup result
-	"""
-	pass
+    If the table is a dict, roll dice and lookup result
+    """
+    pass
