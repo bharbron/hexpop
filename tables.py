2f212372d00eccca19a81e86cf30f01c8be2b356
Brian Harbron <bharbron@grubhub.com>
2018-08-01 09:43:30 -0500

    Filling in sample data to tables

---
+++ tables.py
@@ -0,0 +1,225 @@
+ADVENTURE_HOOKS = [
+	"Free (roll on Encounters-Intelligent or Animals)",
+	"Slay (roll on Encounters)",
+	"Capture (roll on Encounters or Animals)",
+	"Safely transport (d4, roll on 1 Encounters 2 Animals 3-4 Objects)",
+	"Locate (d6, roll on 1-Encounters 2-Instant Interior Rooms 3-Instant Forest Landmarks 4-Instant Garden Landmarks 5 Animals 6 Objects)",
+	"Acquire (roll on Objects or Animals)",
+	"Destroy (roll on Objects)",
+	"Seduce (roll on Encounters-Intelligent)",
+	"Frame (roll on Encounters-Intelligent or Animals) for a crime",
+	"Spy on (roll on Encounters-Intelligent)",
+	"Re-roll, action must be performed during (roll Event)",
+	"Map (d20 1-Castle Cachtice 2-Castle Poenari 3-8 Garden 9-14 Forest 15-20 Interior.)",
+	"Investigate Murder of (roll on Encounters- Intelligent)",
+	"Investigate Theft of (roll on Objects) belonging to (Encounters-Intelligent)",
+	"Prevent (roll on Event)",
+	"Ruin (Event)",
+	"Secretly sell (roll on Objects) to (roll on Encounters-Intelligent)",
+	"Recruit (roll on Encounters-Intelligent)",
+	"Defend (roll on Location)",
+	"Impersonate (roll on Encounters- Intelligent) during (roll on Event)"
+]
+
+ANIMALS = [
+	"Mouse",
+	"Bat",
+	"Frog",
+	"Loach",
+	"Tortoise",
+	"Crow",
+	"Magpie",
+	"Lorry",
+	"Maggot",
+	"Duck",
+	"Badger",
+	"Mongoose",
+	"Lizard",
+	"Frilled lizard",
+	"Salamander",
+	"Tamarin",
+	"Spider",
+	"Squirrel",
+	"Mole",
+	"Scorpion",
+	"Eel",
+	"Snake",
+	"Armadillo",
+	"Lemur",
+	"Otter",
+	"Rat",
+	"Cat",
+	"Fox",
+	"Sloth",
+	"Swan",
+	"Hedgehog",
+	"Chameleon",
+	"Ferret",
+	"Centipede",
+	"Mantis",
+	"Firefly ",
+	"Weasel ",
+	"Lobster ",
+	"Duck ",
+	"Hedgehog ",
+	"Rabbit ",
+	"Hare ",
+	"Shrike ",
+	"Beetle ",
+	"Goldfish ",
+	"Crab",
+	"Leech",
+	"Butterfly/Moth",
+	"Amoeba",
+	"Bream",
+	"Ray",
+	"Lamprey",
+	"Wolf",
+	"Stag",
+	"Hog",
+	"Vulture",
+	"Tiger ",
+	"Lion",
+	"Leopard ",
+	"Panther ",
+	"Ocelot ",
+	"Boar",
+	"Cheetah",
+	"Crane",
+	"Alligator/Crocodile ",
+	"Dog",
+	"Elephant",
+	"Jackal",
+	"Goat",
+	"Octopus",
+	"Horse",
+	"Hyena",
+	"Jaguar",
+	"Warthog",
+	"Owl",
+	"Panda",
+	"Rhinoceros",
+	"Zebra",
+	"Whale",
+	"Wolverine",
+	"Bear",
+	"Shark",
+	"Gorilla",
+	"Peacock",
+	"Hammerhead shark",
+	"Lynx",
+	"Boa Constrictor",
+	"Yak",
+	"Giraffe or T-Rex",
+	"Hippo or Pterodactyl",
+	"Kangaroo or Brontosaurus",
+	"Ostrich or Sabretoothed Tiger",
+	"Penguin or Mastodon",
+	"Swordfish",
+	"Coelacanth",
+	"Granjoro Jellyfish (10’ diameter, look it up)",
+	"Pangolin",
+	"Komodo Dragon",
+	"Squid",
+	"Chimpanzee/Orangutan"
+]
+
+ENCOUNTERS_INTERIOR = {
+	"Bat/Undead bird Swarm",
+	"Little Crocodile",
+	"Ozwick",
+	"Sleeper",
+	"Ordinary Human",
+	"Goblins, Hobgoblins, etc.",
+	"Footmen in Livery",
+	"Cheshire Cat",
+	"Mome Rath",
+	"Wild Boar",
+	"Wolf or Tiger",
+	"The Rabbit",
+	"March Hare",
+	"Tove",
+	"Pseudoturtle",
+	"Colorless Pawn",
+	"Pale Pawn",
+	"Red Pawn",
+	"Rose Nageire",
+	"Tiger Lily Nageire",
+	"Violet Nageire",
+	"Order of Spades",
+	"Order of Hearts",
+	"Order of Diamonds",
+	"Order of Clubs",
+	"Jub",
+	"Pudding",
+	"Unicorn",
+	"Witch (any old witch)",
+	"Colorless Knight",
+	"Pale Knight",
+	"Red Knight",
+	"Great Grey Grub",
+	"Colorless Bishop",
+	"Pale Bishop",
+	"Red Bishop",
+	"Duchess",
+	"Knave of Hearts",
+	"Hatter",
+	"Pale Rook",
+	"Colorless Rook",
+	"Red Rook",
+	"Sphinx",
+	"Red Bride (Ilona)",
+	"Red Bride (Tizala)",
+	"Red Bride (Ildana)",
+	"King of Hearts",
+	"Guest",
+	"Pale King",
+	"Colorless Queen",
+	"Heart Queen",
+	"RedKing",
+	"Jabberwock"
+}
+
+ENCOUNTERS_GARDEN_NIGHT = {}
+
+ENCOUNTERS_GARDEN_DAY = {}
+
+ENCOUNTERS_FOREST_NIGHT = {}
+
+ENCOUNTERS_FOREST_DAY = {}
+
+ENCOUNTERS_HOUSE_OF_HEARTS = {}
+
+ENCOUNTERS_PALE_HOUSE = {}
+
+ENCOUNTERS_RED_HOUSE = {}
+
+ENCOUNTERS_COLORLESS_HOUSE = {}
+
+ENCOUNTERS_INTELLIGENT = {}
+
+EVENTS = {}
+
+FOREST_LANDMARKS = []
+
+GARDEN_LANDMARKS = []
+
+INTERIOR_ROOMS = []
+
+COMPLICATIONS = []
+
+LOCATIONS = {}
+
+OBJECTS = []
+
+PERPLEXITIES_COMMON = []
+
+PERPLEXITIES_UNCOMMON = []
+
+NPC_RELATIONSHIPS = {}
+
+DOING_RELATIONSHIPS = []
+
+DOING_WANTS = []
+
+DOING_CARES = []
