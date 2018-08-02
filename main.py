66896637e8f44110217c5456564024104be82475
Brian Harbron <bharbron@grubhub.com>
2018-08-02 12:35:43 -0500

    Fixed NoneType exception

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import re
+import pdb
 
 from random import randint
 
@@ -16,29 +17,20 @@ def parse_and_replace(text, tables, depth=0, max_depth=MAX_DEPTH):
 
     Returns the updated string, or the original text if no {{TABLE_REFERENCE[s]}} found
     """
-#    print "depth = {0}".format(depth)
-#    print "max_depth = {0}".format(max_depth)
     if depth >= max_depth:
-#        print "too deep"
         return text
-#    print "text = {0}".format(text)
     table_references = re.search(r"{{([A-Za-z0-9_]+)}}", text)
-#    print "table_references = {0}".format(table_references)
     if table_references:
-        table_name = table_references.group(1)
-
-        # TODO: Fix this so it will loop until finding the first non-restricted keyword
-        if table_name not in RESTRICTED_KEYWORDS:
-            table_text = roll_on_table(tables[table_name])
-            # Dive down into the new table result to look for more TABLE_REFERENCE[s]
-            table_text = parse_and_replace(table_text, tables, depth=depth+1)
-            updated_text = re.sub(r"{{[A-Za-z0-9_]+}}", table_text, text, count=1)
-            # Continue across the same text to find more TABLE_REFERENCES[s] at the same depth
-            return parse_and_replace(updated_text, tables, depth=depth)
-        else:
-            return parse_and_replace(text, tables, depth=depth)
-    else:
-        return text
+        for table_name in table_references.groups():
+            if table_name not in RESTRICTED_KEYWORDS:
+                table_text = roll_on_table(tables[table_name])
+                # Dive down into the new table result to look for more TABLE_REFERENCE[s]
+                table_text = parse_and_replace(table_text, tables, depth=depth+1)
+                updated_text = text.replace(r"{{" + table_name + r"}}", table_text, 1)
+                # updated_text = re.sub(r"{{[A-Za-z0-9_]+}}", table_text, text, count=1)
+                # Continue across the same text to find more TABLE_REFERENCES[s] at the same depth
+                return parse_and_replace(updated_text, tables, depth=depth)
+    return text
 
 def roll_on_table(table):
     """
@@ -52,7 +44,6 @@ def roll_on_table(table):
         return table[randint(0, len(table)) - 1]
     if type(table) is dict:
         results = sorted([int(key) for key in table.keys()])
-#        print "results = {0}".format(results)
         max_roll = results[-1]
         roll = randint(1, max_roll)
         for result in results:
