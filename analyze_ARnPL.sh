b01eb296683caaf7ae61cd67826012ffd68fce03
Brian Harbron <bharbron@grubhub.com>
2018-08-07 17:04:46 -0500

    Fixed analyze script based on recent changes to territory icons

--- analyze_ARnPL.sh
+++ analyze_ARnPL.sh
@@ -1,14 +1,14 @@
 python hexpop.py --npcs ARnPL_npcs.json ARnPL_map.json ARnPL_tables.json > ARnPL_analyze.html
-echo "Adventure Hooks (<p>&#9758;)"
-grep -o "<p>&#9758;" ARnPL_analyze.html | wc -l
+echo "Adventure Hooks (adventure_hook)"
+grep -o "adventure_hook" ARnPL_analyze.html | wc -l
 echo "Total Landmarks (<h3>)"
 grep -o "<h3>" ARnPL_analyze.html | wc -l
 echo "Day Encounters (<strong>day)"
 grep -o "<strong>day" ARnPL_analyze.html | wc -l
 echo "Night Encounters (<strong>night)"
 grep -o "<strong>night" ARnPL_analyze.html | wc -l
-echo "Interiors (<h3>Interior) [encounters roughly 1/2 this]"
-grep -o "<h3>Interior" ARnPL_analyze.html | wc -l
+echo "Interiors (interior_icon) [encounters roughly 1/2 this]"
+grep -o "interior_icon" ARnPL_analyze.html | wc -l
 echo "NPCs carrying items (Carrying)"
 grep -p "Carrying" ARnPL_analyze.html | wc -l
 echo "All Treasures (<p>Contains)"
@@ -19,8 +19,8 @@ echo "Magical Items (<p>Contains magical)"
 grep -o "<p>Contains magical" ARnPL_analyze.html | wc -l
 echo "Rare Treasures (<p>Contains rare)"
 grep -o "<p>Contains rare" ARnPL_analyze.html | wc -l
-echo "Coins or Jewelry (<p>Contains Coins) [only in treasures]"
-grep -o "<p>Contains Coins" ARnPL_analyze.html | wc -l
+echo "Coins or Jewelry (<p>Contains coins) [only in treasures]"
+grep -o "<p>Contains coins" ARnPL_analyze.html | wc -l
 echo "All Spellbooks (Spell)"
 grep -o "Spell" ARnPL_analyze.html | wc -l
 echo "Hex references (<strong>square)"
